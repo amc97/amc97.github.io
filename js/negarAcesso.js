c3b7d26854b2093af3344ca49f37b262860473c5
Anderson Malheiro <malheiroanderson97@gmail.com>
2017-10-23 15:26:05 -0300

    novo site

--- js/negarAcesso.js
+++
@@ -1,7 +0,0 @@
-var auth = firebase.auth();
-
-auth.onAuthStateChanged(function (login) {
-    if (!login) {
-      location.assign('index.html');
-    }
-});
--- vendor/firebase/firebase.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    apiKey: "AIzaSyBbvZoJs93MNbs7sFPjzJCiaNb7zPg-dR8",
-    authDomain: "appplantao-d289e.firebaseapp.com",
-    databaseURL: "https://appplantao-d289e.firebaseio.com",
-    projectId: "appplantao-d289e",
-    storageBucket: "",
-    messagingSenderId: "201003697936"
-};
-firebase.initializeApp(config);
